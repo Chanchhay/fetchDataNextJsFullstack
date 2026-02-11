377177ab8d3e5b3a9788f28eb0480e38e9998af1
chanchhay <chanchhay07@gmail.com>
2026-02-11 10:18:07 +0700

    updated-modal

--- src/app/@modal/(.)users/user-detail/[id]/page.tsx
+++ src/app/@modal/(.)users/user-detail/[id]/page.tsx
@@ -1,6 +1,7 @@
 import ProductLoading from "@/components/i-tech-loading/loading";
+import Modal from "@/components/i-tech-modal/Modal";
+import { SkeletonCard } from "@/components/i-tech-skeleton/Skeleton-card";
 import { loadUserDetail } from "@/lib/fetchData";
-import Image from "next/image";
 import { Suspense } from "react";
 
 export default async function Page({
@@ -10,29 +11,5 @@ export default async function Page({
 }) {
     const { id } = await params;
     const user = await loadUserDetail(id);
-    return (
-        <Suspense fallback={<h1>loading...</h1>}>
-            <div className="h-full w-full flex items-center justify-center">
-                <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
-                    <div className="text-center">
-                        <h3 className="text-2xl font-bold text-gray-900">
-                            {user.name}
-                        </h3>
-                        <div className="mt-2 px-7 py-3">
-                            <p className="text-lg text-gray-500">
-                                {user.email}
-                            </p>
-                            <Image
-                                alt="user-image"
-                                src={user.avatar}
-                                width={600}
-                                height={600}
-                                unoptimized
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Suspense>
-    );
+    return <Modal user={user} />;
 }
---
+++ src/components/i-tech-modal/Modal.tsx
@@ -0,0 +1,40 @@
+"use client";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from "../ui/dialog";
+import { Suspense } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { SkeletonCard } from "../i-tech-skeleton/Skeleton-card";
+
+type User = {
+    name: string;
+    email: string;
+    avatar: string;
+};
+export default function Modal({ user }: { user: User }) {
+    const router = useRouter();
+    return (
+        <Dialog open={true} onOpenChange={() => router.back()}>
+            <Suspense fallback={<SkeletonCard />}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>{user.name}</DialogTitle>
+                        <DialogDescription>{user.email}</DialogDescription>
+                        <Image
+                            src={user.avatar}
+                            alt="user-image"
+                            width={600}
+                            height={400}
+                            unoptimized
+                        />
+                    </DialogHeader>
+                </DialogContent>
+            </Suspense>
+        </Dialog>
+    );
+}
